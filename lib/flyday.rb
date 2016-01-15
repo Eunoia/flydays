ef365cefbc45b5492ff7635f5b75d44a726a30d2
Evan R <eunoia.github@gmail.com>
2016-01-15 13:24:09 -0800

    hard code the cookie, clean up time

--- lib/flyday.rb
+++ lib/flyday.rb
@@ -13,67 +13,10 @@ class Flyday
     @mechanize.log = Logger.new "flyday.log"
   end
 
-  def search_hash
-    {
-      "flowName" => "book",
-      "omni_reload" => "false",
-      "twoWayTrip" => "false",
-      "returnAirport" => " ",
-      "returnAirport_displayed" => " ",
-      "fareType" => "DOLLARS",
-      "originAirport" => "CAK",
-      "originAirport_displayed" => "Akron-Canton, OH - CAK",
-      "destinationAirport" => "ATL",
-      "destinationAirport_displayed" => "Atlanta, GA - ATL",
-      "outboundDateString" => "01/15/2016",
-      "omni_outboundDateString" => "01/15/2016",
-      "returnDateString" => " ",
-      "omni_returnDateString" => "01/16/2016",
-      "outboundTimeOfDay" => "ANYTIME",
-      "returnTimeOfDay" => " ",
-      "adultPassengerCount" => "1",
-      "seniorPassengerCount" => "0",
-      "promoCode" => " ",
-      "serviceID" => "flightoptions",
-      "appID" => "swa",
-      "appver" => "2.19.0",
-      "channel" => "wap",
-      "platform" => "thinclient",
-      "cacheid" => " ",
-      "rcid" => "spaiphone",
-    }
-  end
-
   def search(date)
-    # @mechanize.get('https://southwest.com/')
-    # post = @mechanize.post('https://southwest.com/flight/search-flight.html',{
-    #   'originAirport' => 'MDW',
-    #   'twoWayTrip' => 'false',
-    #   'promoCode ' => '',
-    #   'returnDateString'  => '',
-    #   'destinationAirport' => 'XYZ',
-    #   'outboundDateString' => '01/14/2016',
-    #   'adultPassengerCount' => '1',
-    #   'seniorPassengerCount' => '0',
-    #   'fareType' => 'DOLLARS',
-    # })
-    @mechanize.get('https://mobile.southwest.com')
-    post = @mechanize.post('https://mobile.southwest.com/middleware/MWServlet',{
-      serviceID: 'getTravelInfo',
-      appID: 'swa',
-      appver: '2.19.0',
-      channel: 'wap',
-      platform: 'thinclient',
-      cacheid:' ',
-      rcid: 'spaiphone'
-    }, {"Content-Type" => "application/x-www-form-urlencoded"})
-    # binding.pry
-    File.write("hello.html", post.body)
     post = @mechanize.post('https://mobile.southwest.com/middleware/MWServlet',
-    # 'flowName=book&omni_reload=false&twoWayTrip=false&returnAirport=&returnAirport_displayed=&fareType=DOLLARS&originAirport=CAK&originAirport_displayed=Akron-Canton%2C%20OH%20-%20CAK&destinationAirport=ATL&destinationAirport_displayed=Atlanta%2C%20GA%20-%20ATL&outboundDateString=01%2F15%2F2016&omni_outboundDateString=01%2F15%2F2016&returnDateString=&omni_returnDateString=01%2F16%2F2016&outboundTimeOfDay=ANYTIME&returnTimeOfDay=&adultPassengerCount=1&seniorPassengerCount=0&promoCode=&serviceID=flightoptions&appID=swa&appver=2.19.0&channel=wap&platform=thinclient&cacheid=&rcid=spaiphone',
-    search_hash,
-    {"Content-Type" => "application/x-www-form-urlencoded"})
-    # File.write("hello.html", post.body)
-    []
+    'flowName=book&omni_reload=false&twoWayTrip=false&returnAirport=&returnAirport_displayed=&fareType=DOLLARS&originAirport=MDW&originAirport_displayed=Chicago%20(Midway)%2C%20IL%20-%20MDW&destinationAirport=ATL&destinationAirport_displayed=Atlanta%2C%20GA%20-%20ATL&outboundDateString=01%2F19%2F2016&omni_outboundDateString=01%2F19%2F2016&returnDateString=&omni_returnDateString=01%2F20%2F2016&outboundTimeOfDay=ANYTIME&returnTimeOfDay=&adultPassengerCount=1&seniorPassengerCount=0&promoCode=&serviceID=flightoptions&appID=swa&appver=2.19.0&channel=wap&platform=thinclient&cacheid=&rcid=spaiphone&',
+    { 'Cookie' => 'cacheid=166fd301a3d-9f6b-40c0-9aa7-0b25dc9bb862;' })
+    post.body
   end
 end
