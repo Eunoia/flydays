a9b06d1cfd90dfc998bc30a19fc12f97227b3b20
Evan R <eunoia.github@gmail.com>
2016-01-21 18:18:43 -0800

    Uppercase env variable

--- lib/flyday.rb
+++ lib/flyday.rb
@@ -18,8 +18,8 @@ class Flyday
 
   def search(date=Date.today, seats: 1)
     post = @mechanize.post('https://mobile.southwest.com/middleware/MWServlet',
-    { 'Cookie' => "cacheid=#{ENV["cacheid"]};" })
     "flowName=book&omni_reload=false&twoWayTrip=false&returnAirport=&returnAirport_displayed=&fareType=DOLLARS&originAirport=MDW&originAirport_displayed=Chicago%20(Midway)%2C%20IL%20-%20MDW&destinationAirport=ATL&destinationAirport_displayed=Atlanta%2C%20GA%20-%20ATL&outboundDateString=#{date.strftime("%m%%2F%d%%2F%Y")}&omni_outboundDateString=01%2F19%2F2016&returnDateString=&omni_returnDateString=01%2F20%2F2016&outboundTimeOfDay=ANYTIME&returnTimeOfDay=&adultPassengerCount=#{seats}&seniorPassengerCount=0&promoCode=&serviceID=flightoptions&appID=swa&appver=2.19.0&channel=wap&platform=thinclient&cacheid=&rcid=spaiphone&",
+    { 'Cookie' => "cacheid=#{ENV["CACHEID"]};" })
     json = JSON.parse(post.body)
     json['out_flight_options']
   end
